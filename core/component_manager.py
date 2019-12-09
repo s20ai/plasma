f21ac3c1d6ab596f4390df7a9ac7e26b5b465136
Tabish Imran <tabish.imran96@gmail.com>
2019-12-08 21:49:55 -0800

    Added component manager boiler plate code

---
+++ core/component_manager.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def download_component():
+    raise NotImplementedError
+
+
+def list_components():
+    raise NotImplementedError
+
+
+def search_components():
+    raise NotImplementedError
