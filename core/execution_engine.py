d0d66df914dafb27af5bf060bfe04efa6efd1f71
Tabish Imran <tabish.imran96@gmail.com>
2019-12-18 01:44:02 -0800

    core algorithm and logging

--- core/execution_engine.py
+++ core/execution_engine.py
@@ -2,21 +2,39 @@
 
 import logging
 
+logger = logging.getLogger("WXE")
 
-def load_workflow_file():
+def load_workflow_file(file_path):
+    logger.info('parsing workflow file')
     raise NotImplementedError
 
-def validate_workflow_file():
+def validate_workflow(workflow):
+    logger.info('validating workflow file')
     raise NotImplementedError
 
 def verify_components():
+    logger.info('verifying components')
     raise NotImplementedError
 
 def generate_workflow_requirements():
+    logger.info('generating workflow requirements')
     raise NotImplementedError
 
 def setup_virtual_environment():
+    logger.info('setting up virtual environment')
     raise NotImplementedError
 
 def execute_workflow():
+    logger.info('executing workflow')
     raise NotImplementedError
+
+def run_workflow(file_path):
+    workflow = load_workflow_file(file_path)
+    workflow_valid = validate_workflow(workflow)
+    if workflow_valid:
+        requirements = generate_workflow_requirements(workflow)
+        virtual_environment = setup_virtual_environment(requirements)
+        execute_workflow(workflow,virtual_environment)
+    else:
+        logger.error('Invalid Workflow')
+        exit(1)
