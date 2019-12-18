6b462c91eec74b4f248c88124ca39477c886e991
Tabish Imran <tabish.imran96@gmail.com>
2019-12-18 02:23:03 -0800

    Implemented load_workflow

--- core/execution_engine.py
+++ core/execution_engine.py
@@ -1,40 +1,58 @@
 #!/usr/bin/python3
 
 import logging
+from core.utils import get_config
+import yaml
 
 logger = logging.getLogger("WXE")
 
-def load_workflow_file(file_path):
-    logger.info('parsing workflow file')
-    raise NotImplementedError
+
+def load_workflow_file(workflow_name):
+    try:
+        logger.info('parsing workflow file')
+        config = get_config()
+        workflow_path = config['workflows_path'] + workflow_name
+        with open(workflow_path) as workflow:
+            yaml.load(workflow, Loader=yaml.FullLoader)
+        return workflow
+    except FileNotFoundError:
+        logger.error('Unable to execute workflow : File Not Found')
+    except Error as e:
+        logger.error('Unable to execute workflow :'+str(e))
+
 
 def validate_workflow(workflow):
     logger.info('validating workflow file')
     raise NotImplementedError
 
+
 def verify_components():
     logger.info('verifying components')
     raise NotImplementedError
+
 
 def generate_workflow_requirements():
     logger.info('generating workflow requirements')
     raise NotImplementedError
 
+
 def setup_virtual_environment():
     logger.info('setting up virtual environment')
     raise NotImplementedError
+
 
 def execute_workflow():
     logger.info('executing workflow')
     raise NotImplementedError
 
-def run_workflow(file_path):
-    workflow = load_workflow_file(file_path)
+
+def run_workflow(workflow_name):
+    workflow = load_workflow_file(workflow_name)
     workflow_valid = validate_workflow(workflow)
     if workflow_valid:
         requirements = generate_workflow_requirements(workflow)
         virtual_environment = setup_virtual_environment(requirements)
-        execute_workflow(workflow,virtual_environment)
+        execute_workflow(workflow, virtual_environment)
     else:
         logger.error('Invalid Workflow')
         exit(1)
