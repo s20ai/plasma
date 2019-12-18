2a58660d02872e7758202297934a2f6e02acb967
Tabish Imran <tabish.imran96@gmail.com>
2019-12-18 04:17:08 -0800

    Implemented validate_workflow and verify_components

--- core/execution_engine.py
+++ core/execution_engine.py
@@ -3,34 +3,60 @@
 import logging
 from core.utils import get_config
 import yaml
+import os
 
 logger = logging.getLogger("WXE")
-
+plasma_config = get_config()
 
 def load_workflow_file(workflow_name):
     try:
         logger.info('loading workflow file')
         config = get_config()
         workflow_path = config['workflows_path'] + workflow_name
-        with open(workflow_path) as workflow:
-            yaml.load(workflow, Loader=yaml.FullLoader)
+        with open(workflow_path) as workflow_file:
+            workflow = yaml.load(workflow_file, Loader=yaml.FullLoader)
         return workflow
     except FileNotFoundError:
-        logger.error('Unable to execute workflow : File Not Found')
+        logger.error('unable to execute workflow : File Not Found')
         exit(1)
-    except Error as e:
-        logger.error('Unable to execute workflow :'+str(e))
+    except yaml.scanner.ScannerError as e:
+        logger.error('unable to execute workflow : YAML Invalid')
+        logger.error(e)
+        exit(1)
+    except Exception as e:
+        logger.error('unable to execute workflow :'+str(e))
         exit(1)
 
 
 def validate_workflow(workflow):
     logger.info('validating workflow file')
-    raise NotImplementedError
+    workflow_keys = list(workflow.keys())
+    required_keys = ["name","description","version","workflow"]
+    for key in required_keys:
+        if key not in workflow_keys:
+            logger.error('unable to execute workflow')
+            logger.error('missing key : '+str(key))
+            exit(1)
+    verified = verify_components(workflow)
+    return verified
+    
 
 
-def verify_components():
+def verify_components(workflow):
     logger.info('verifying components')
-    raise NotImplementedError
+    components_path = plasma_config['components_path']
+    local_components = os.listdir(components_path)
+    workflow_components = list(workflow['workflow'].keys())
+    for component in workflow_components:
+        if component not in local_components:
+            logger.error('unable to execute workflow')
+            logger.error('component not found : '+component)
+            verified = False
+            break
+        else:
+            verified = True
+    return verified
+
 
 
 def generate_workflow_requirements():
@@ -56,5 +82,5 @@ def run_workflow(workflow_name):
         virtual_environment = setup_virtual_environment(requirements)
         execute_workflow(workflow, virtual_environment)
     else:
-        logger.error('Invalid Workflow')
+        logger.error('invalid Workflow')
         exit(1)
