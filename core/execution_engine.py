d6f2ff8e29567432e2af02f55988d67bdeab009b
Tabish Imran <tabish.imran96@gmail.com>
2019-12-18 00:12:44 -0800

    restructured boilerplate code

--- core/execution_engine.py
+++ core/execution_engine.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 
+import logging
 
 
+def load_workflow_file():
+    raise NotImplementedError
+
+def validate_workflow_file():
+    raise NotImplementedError
+
+def verify_components():
+    raise NotImplementedError
+
+def generate_workflow_requirements():
+    raise NotImplementedError
+
+def setup_virtual_environment():
+    raise NotImplementedError
+
+def execute_workflow():
+    raise NotImplementedError
