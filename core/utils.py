219b4487deca5a03c7be32a8c7ade3ab85343330
Tabish Imran <tabish.imran96@gmail.com>
2019-12-20 02:23:37 -0800

    Exit statements

--- core/utils.py
+++ core/utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 def create_plasma_project(project_name):
-    plasma_path = os.getcwd()+'/'+project_name
+    plasma_path = os.getcwd()+'/'+project_name+'/'
     if os.path.isdir(plasma_path):
         print('> directory already exists')
     else:
@@ -38,9 +38,11 @@ def get_config():
             plasma_config = json.load(config_file)
         return plasma_config
     except FileNotFoundError:
-        print("> the current directory doesn't have a plasma_project")
+        print("\n> the current directory doesn't have a plasma_project\n")
+        exit(1)
     except Exception as e:
         print("> failed to load plasma_config.json")
+        exit(1)
 
 
 def parse_workflow(workflow):
