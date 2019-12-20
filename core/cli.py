9f34c4c10275164fa47def119a8c633b698ac62f
Tabish Imran <tabish.imran96@gmail.com>
2019-12-20 01:51:09 -0800

    Decided to not do the whole command dispatcher thing Revert "remaned command_dispatcher to cli ( moving command_dispatcher to"
    This reverts commit c1edac82129829e3367b66a3ea527c40de2dea24.

--- core/cli.py
+++ core/cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-from core.command_dispatcher import execute
+import core.component_manager as component_manager
+import core.workflow_manager as workflow_manager
+import core.execution_engine as execution_engine
 import click
 import os
 import collections
@@ -17,21 +19,21 @@ class OrderedGroup(click.Group):
 
 
 @click.group(cls=OrderedGroup)
-def cli():
+def plasma_cli():
     pass
 
 
-# Plasma init
+# Plasma init 
 
 @click.argument('project_name', required=True)
 @click.command(name="init", help="initialize a plasma project", )
 def initialize_project(project_name):
-    execute('setup_plasma_project', project_name)
+    raise NotImplementedError
 
 
 # Model command group
 
-@click.group(cls=OrderedGroup, help="manage connected models")
+@click.group(cls=OrderedGroup,help="manage connected models")
 def model():
     pass
 
@@ -52,76 +54,92 @@ def serve_model():
 def monitor_model():
     raise NotImplementedError
 
-
 # Workflow command group
 
-@click.group(cls=OrderedGroup, help="manage plasma workflows")
+
+@click.group(cls=OrderedGroup,help="manage plasma workflows")
 def workflow():
     pass
+
+
+@click.command(name="list", help="list existing workflows")
+def list_workflow():
+    workflow_manager.list_workflows()
 
 
 @click.command(name="run", help="run workflow")
 @click.argument('workflow_name', required=True)
 def run_workflow(workflow_name):
     print('\n> Running workflow '+workflow_name+'\n')
-    execute('run_workflow', workflow_name)
+    workflow_manager.run_workflow(workflow_name)
+
+
+@click.command(name="describe", help="describe workflow")
+@click.argument('workflow_name', required=True)
+def describe_workflow(workflow_name):
+    workflow_manager.describe_workflow(workflow_name)
 
 
 @click.command(name="schedule", help="schedule workflows")
 @click.argument('workflow_name', required=True)
 def schedule_workflow(workflow_name):
-    execute('schedule_workflow', workflow_name)
+    os.system('crontab -e')
 
 
 # Component command group
 
-@click.group(cls=OrderedGroup, help="manage plasma components")
+@click.group(cls=OrderedGroup,help="manage plasma components")
 def component():
     pass
 
 
 @click.command(name="list", help="list components")
 def list_component():
-    execute('list_components')
+    component_manager.list_components()
 
 
 @click.command(name="search", help="search for components")
 @click.argument('component_name', required=True)
 def search_component(component_name):
-    execute('search_components', component_name)
+    component_manager.search_components(component_name)
 
 
 @click.command(name="get", help="download components")
 @click.argument('component_name', required=True)
 def get_component(component_name):
-    execute('download_component', component_name)
+    component_manager.download_component(component_name)
 
 
 @click.command(name="describe", help="download components")
 @click.argument('component_name', required=True)
 def describe_component(component_name):
-    execute('describe_component', component_name)
+    component_manager.describe_component(component_name)
 
 
 @click.command(name="run", help="run components")
 @click.argument('component_name', required=True)
 @click.option("--parameters", "-p", multiple=True)
 def run_component(component_name, parameters):
-    execute('run_component', [component_name, parameters])
+    # output = execution_engine.run_component(component_name,
+           # parameters)
+    # print(output)
+    raise NotImplementedError
 
 
-def plasma_cli():
+def command_dispatcher():
     model.add_command(list_model)
     model.add_command(serve_model)
     model.add_command(monitor_model)
+    workflow.add_command(list_workflow)
     workflow.add_command(run_workflow)
+    workflow.add_command(describe_workflow)
     workflow.add_command(schedule_workflow)
     component.add_command(list_component)
     component.add_command(search_component)
     component.add_command(get_component)
     component.add_command(describe_component)
-    cli.add_command(initialize_project)
-    cli.add_command(component)
-    cli.add_command(workflow)
-    cli.add_command(model)
-    return cli
+    plasma_cli.add_command(initialize_project)
+    plasma_cli.add_command(component)
+    plasma_cli.add_command(workflow)
+    plasma_cli.add_command(model)
+    return plasma_cli
--- core/utils.py
+++ core/utils.py
@@ -3,10 +3,12 @@ import os
 from pathlib import Path
 
 
-def create_plasma_project(project_name):
-    plasma_path = os.getcwd()+'/'+project_name
+def setup_plasma():
+    home = str(Path.home())
+    plasma_path = home+'/.plasma/'
     if os.path.isdir(plasma_path):
-        print('> directory already exists')
+        with open(plasma_path+'plasma_config.json') as config_file:
+            plasma_config = json.load(config_file)
     else:
         plasma_config = {}
         log_path = plasma_path + 'logs/'
@@ -32,15 +34,11 @@ def create_plasma_project(project_name):
 
 
 def get_config():
-    try:
-        config_file_path = './plasma_config.json'
-        with open(config_file_path) as config_file:
-            plasma_config = json.load(config_file)
-        return plasma_config
-    except FileNotFoundError:
-        print("> the current directory doesn't have a plasma_project")
-    except Exception as e:
-        print("> failed to load plasma_config.json")
+    home = str(Path.home())
+    config_file_path = home+'/.plasma/plasma_config.json'
+    with open(config_file_path) as config_file:
+        plasma_config = json.load(config_file)
+    return plasma_config
 
 
 def parse_workflow(workflow):
